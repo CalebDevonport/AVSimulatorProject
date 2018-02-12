f0f01cc26d512466ecc66a7884038426e4c795f0
Diana Toader <dianaelenatoader@gmail.com>
2018-02-12 19:29:21 +0000

    RimDriverSimModel - done

--- src/main/java/aim4/driver/rim/RimDriverSimModel.java
+++ src/main/java/aim4/driver/rim/RimDriverSimModel.java
@@ -1,18 +1,41 @@
 package aim4.driver.rim;
 
 import aim4.driver.DriverSimModel;
+import aim4.map.Road;
 import aim4.map.aim.AIMSpawnPoint;
 
 public interface RimDriverSimModel extends DriverSimModel {
+    /////////////////////////////////
+    // PUBLIC METHODS
+    /////////////////////////////////
+
+    // origin and destination
+
     /**
-     * Sets where the driver agent is coming from
-     * @param spawnPoint the spawn point that generated the driver.
+     * Get where this driver is coming from.
+     *
+     * @return the Road where this driver is coming from
+     */
+    AIMSpawnPoint getSpawnPoint();
+
+    /**
+     * Set where this driver agent is coming from.
+     *
+     * @param spawnPoint the spawn point that generated the driver
      */
     void setSpawnPoint(AIMSpawnPoint spawnPoint);
 
     /**
-     * Returns a String representing the state of the Driver.
-     * @return
+     * Get where this driver is going.
+     *
+     * @return the Road where this driver is going
      */
-    String getStateString();
+    Road getDestination();
+
+    /**
+     * Set where this driver is going.
+     *
+     * @param destination the Road where this driver should go
+     */
+    void setDestination(Road destination);
 }
